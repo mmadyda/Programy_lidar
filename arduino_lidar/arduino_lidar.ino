26b72538b7e0b59390ae0754233ff6cbd5711067
mmadyda <63304339+mmadyda@users.noreply.github.com>
2022-04-06 20:38:59 +0200

    format kodu

--- arduino_lidar/arduino_lidar.ino
+++ arduino_lidar/arduino_lidar.ino
@@ -1,57 +1,21 @@
 /*
- * RoboPeak RPLIDAR Arduino Example
- * This example shows the easy and common way to fetch data from an RPLIDAR
- * 
- * You may freely add your application code based on this template
- *
- * USAGE:
- * ---------------------------------
- * 1. Download this sketch code to your Arduino board
- * 2. Connect the RPLIDAR's serial port (RX/TX/GND) to your Arduino board (Pin 0 and Pin1)
- * 3. Connect the RPLIDAR's motor ctrl pin to the Arduino board pin 3 
- */
- 
-/* 
-import sys
-
-import serial
-import time
+   Ctrl+T auto format kodu
+   ---------------------------------
+   1. Download this sketch code to your Arduino board
+   2. Connect the RPLIDAR's serial port (RX/TX/GND) to your Arduino board (Pin 0 and Pin1)
+   3. Connect the RPLIDAR's motor ctrl pin to the Arduino board pin 3
+*/
 
 
-ser = serial.Serial()
-ser.port = "COM12"
-ser.baudrate = 115200
-ser.timeout = 1
-ser.setDTR(False)
-ser.parity = serial.PARITY_NONE
-ser.stopbits = serial.STOPBITS_ONE
-
-
-ser.open()
-
-
-
-
-if ser.isOpen() == False:
-    ser.open() # open serial port if not open
-
-while True:
-    print(ser.readline())
-
-ser.close() # close serial port
- *
- */
- 
-// This sketch code is based on the RPLIDAR driver library provided by RoboPeak
 #include <RPLidar.h>
 #include <SoftwareSerial.h>
 
-SoftwareSerial mySerial(2, 3); // RX, TX
+SoftwareSerial mySerial(2, 3); // RX, TX LIDAR
 
 RPLidar lidar;
 
 #define RPLIDAR_MOTOR 4 // The PWM pin for control the speed of RPLIDAR's motor.
-                        // This pin should connected with the RPLIDAR's MOTOCTRL signal 
+// This pin should connected with the RPLIDAR's MOTOCTRL signal
 
 float ODL_1 = 0.6;
 float ODL_2 = 0.8;
@@ -73,7 +37,7 @@ int zakres_8 = 0;
 
 int liczba_skanow = 50;
 
-unsigned long previousMillis = 0; 
+unsigned long previousMillis = 0;
 const long BLINK_INTERVAL = 1000;   // interval at which to blink LED (milliseconds)
 int ledState = LOW;
 float min_dist = 999;
@@ -85,8 +49,8 @@ void dopisz()
     mySerial.print(" odleglosc: ");
     mySerial.print(metry);
     mySerial.print("   ");
-    */
-    mySerial.println(min_dist);                     
+  */
+  mySerial.println(min_dist);
 }
 void setup() {
   // bind the RPLIDAR driver to the arduino hardware serial
@@ -94,7 +58,7 @@ void setup() {
   mySerial.begin(115200);
 
   mySerial.println("Witoj Michał");
-  
+
   // set pin modes
   pinMode(RPLIDAR_MOTOR, OUTPUT);
 }
@@ -103,7 +67,7 @@ void loop() {
   unsigned long currentMillis = millis();
   if (IS_OK(lidar.waitPoint())) {
     float distance = lidar.getCurrentPoint().distance; //distance value in mm unit
-    float metry = distance/1000;
+    float metry = distance / 1000;
     float angle    = lidar.getCurrentPoint().angle; //anglue value in degree
     bool  startBit = lidar.getCurrentPoint().startBit; //whether this point is belong to a new scan
     byte  quality  = lidar.getCurrentPoint().quality; //quality of the current measurement
@@ -111,129 +75,131 @@ void loop() {
     //mySerial.println("Start bit: ");
     //mySerial.println(startBit);
 
-    if(angle > 315 or angle < 45)
-   {
-      if(metry < min_dist and metry != 0)
+    if (angle > 315 or angle < 45)
+    {
+      if (metry < min_dist and metry != 0)
       {
         min_dist = metry;
       }
-   }
+    }
 
-    
-   if(startBit == 1)
-   {
+
+    if (startBit == 1)
+    {
       //mySerial.println("min dist: ");
       mySerial.println(min_dist);
 
-      if(min_dist <= ODL_1 )
-    {
-      
+      if (min_dist <= ODL_1 )
+      {
 
-      if (currentMillis - previousMillis >= BLINK_INTERVAL) {
-        // if the LED is off turn it on and vice-versa:
-        if(ledState == LOW)
+
+        if (currentMillis - previousMillis >= BLINK_INTERVAL) {
+          // if the LED is off turn it on and vice-versa:
+          if (ledState == LOW)
+          {
+            ledState = HIGH;
+          }
+          else {
+            ledState = LOW;
+          }
+
+          // set the LED with the ledState of the variable:
+          mySerial.println(ledState);
+
+          // save the last time you blinked the LED
+          previousMillis = currentMillis;
+        }
+      }
+      else if (min_dist > ODL_1 and min_dist <= ODL_2 )
+      {
+        zakres_2++;
+        if (zakres_2 >= liczba_skanow)
         {
-          ledState = HIGH;
+          dopisz();
+          mySerial.println("2");
+          zakres_2 = 0;
         }
-        else{
-          ledState = LOW;
+      }
+      else if (min_dist > ODL_2  and min_dist <= ODL_3 )
+      {
+        zakres_3++;
+        if (zakres_3 >= liczba_skanow)
+        {
+          dopisz();
+          mySerial.println("3");
+          zakres_3 = 0;
         }
-    
-        // set the LED with the ledState of the variable:
-        mySerial.println(ledState);
-    
-        // save the last time you blinked the LED
-        previousMillis = currentMillis;
       }
-    }
-    else if(min_dist > ODL_1 and min_dist <= ODL_2 )
-    {
-      zakres_2++;
-      if(zakres_2 >= liczba_skanow)
+      else if (min_dist > ODL_3 and min_dist <= ODL_4 )
       {
-        dopisz();
-        mySerial.println("2");
-        zakres_2 = 0;
+        zakres_4++;
+        if (zakres_4 >= liczba_skanow)
+        {
+          dopisz();
+          mySerial.println("4");
+          zakres_4 = 0;
+        }
       }
-    }
-    else if(min_dist > ODL_2  and min_dist <= ODL_3 )
-    {
-      zakres_3++;
-      if(zakres_3 >= liczba_skanow)
+      else if (min_dist > ODL_4 and min_dist <= ODL_5 )
       {
-        dopisz();
-        mySerial.println("3");
-        zakres_3 = 0;
+        zakres_5++;
+        if (zakres_5 >= liczba_skanow)
+        {
+          dopisz();
+          mySerial.println("5");
+          zakres_5 = 0;
+        }
       }
-    }
-    else if(min_dist > ODL_3 and min_dist <= ODL_4 )
-    {
-      zakres_4++;
-      if(zakres_4 >= liczba_skanow)
+      else if (min_dist > ODL_5 and min_dist <= ODL_6 )
       {
-        dopisz();
-        mySerial.println("4");
-        zakres_4 = 0;
+        zakres_6++;
+        if (zakres_6 >= liczba_skanow)
+        {
+          dopisz();
+          mySerial.println("6");
+          zakres_6 = 0;
+        }
       }
-    }
-    else if(min_dist > ODL_4 and min_dist <= ODL_5 )
-    {
-      zakres_5++;
-      if(zakres_5 >= liczba_skanow)
+      else if (min_dist > ODL_6 and min_dist <= ODL_7 )
       {
-        dopisz();
-        mySerial.println("5");
-        zakres_5 = 0;
+        zakres_7++;
+        if (zakres_7 >= liczba_skanow)
+        {
+          dopisz();
+          mySerial.println("7");
+          zakres_7 = 0;
+        }
       }
-    }
-    else if(min_dist > ODL_5 and min_dist <= ODL_6 )
-    {
-      zakres_6++;
-      if(zakres_6 >= liczba_skanow)
+      else if (min_dist > ODL_7 and min_dist <= ODL_8 )
       {
-        dopisz();
-        mySerial.println("6");
-        zakres_6 = 0;
+        zakres_8++;
+        if (zakres_8 >= liczba_skanow)
+        {
+          dopisz();
+          mySerial.println("8");
+          zakres_8 = 0;
+          {
+          }
+
+        }
       }
+      min_dist = 999;
     }
-    else if(min_dist > ODL_6 and min_dist <= ODL_7 )
-    {
-      zakres_7++;
-      if(zakres_7 >= liczba_skanow)
-      {
-        dopisz();
-        mySerial.println("7");
-        zakres_7 = 0;
-      }
-    }
-    else if(min_dist > ODL_7 and min_dist <= ODL_8 )
-    {
-      zakres_8++;
-      if(zakres_8 >= liczba_skanow)
-      {
-        dopisz();
-        mySerial.println("8");
-        zakres_8 = 0;
-      {
-    }
-    
-   }
-    }
-   min_dist = 999;   
-   }
-   
-   } else {
+
+  }
+  else
+  {
     analogWrite(RPLIDAR_MOTOR, 0); //stop the rplidar motor
-    
-    // try to detect RPLIDAR... 
+
+    // try to detect RPLIDAR...
     rplidar_response_device_info_t info;
     if (IS_OK(lidar.getDeviceInfo(info, 100))) {
-       // detected...
-       lidar.startScan();
-       
-       // start motor rotating at max allowed speed
-       analogWrite(RPLIDAR_MOTOR, 255);
-       delay(1000);
+      // detected...
+      lidar.startScan();
+
+      // start motor rotating at max allowed speed
+      analogWrite(RPLIDAR_MOTOR, 255);
+      delay(1000);
     }
   }
 }

727e663f1b6e7aa698a43c53718b27d987ebccac
mmadyda <63304339+mmadyda@users.noreply.github.com>
2022-04-19 17:25:25 +0200

    Update komunikacja.ino

--- nRF24L01/komunikacja/komunikacja.ino
+++ nRF24L01/komunikacja/komunikacja.ino
@@ -8,6 +8,9 @@
 #define CSN_PIN 10
 
 const unsigned int pipe = 519;
+const unsigned int pipeRead = 500;
+const unsigned int pipeWrite = 800;
+
 
 RF24 radio(CE_PIN, CSN_PIN);
 int dane[2];
@@ -17,16 +20,19 @@ void setup() {
   dane[0] = 90;
   Serial.begin(9600);
   radio.begin();
-  radio.openWritingPipe(pipe);
-  radio.openReadingPipe(1, pipe);
-  radio.startListening();
-  radio.write(dane, sizeof(dane));
+  radio.openWritingPipe(pipeWrite);
+  radio.openReadingPipe(1, pipeRead);
+  //adio.startListening();
+  //radio.write(dane, sizeof(dane));
 
 }
 
 void loop() {
+  radio.stopListening();
   radio.write(dane, sizeof(dane));
+  radio.startListening();
   radio.read(msg, sizeof(msg));
+  radio.stopListening();
   Serial.println(msg[0]);
   delay(100);
 } // loop
